d6d52c396246096eeb35b5d10e1bd0d8a2e33bd7
tkabeee <tkabee@gmail.com>
2017-12-20 16:16:57 +0900

    add root test

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_tile = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_tile}"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
